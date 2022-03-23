8c8d4e4ef180f3beb1d833db225c3644fd1a4c4e
DulceEly <icc.dulce.caldino@gmail.com>
2022-03-23 16:02:21 -0700

    commit last update

--- assignment5 (2)/beginning/js/script.js
+++ assignment5 (2)/beginning/js/script.js
@@ -1,7 +1,7 @@
-let form  = document.getElementById('empForm'); 
-let id = document.getElementById('idEmployee');
-let name = document.getElementById('fullName');
-let ext = document.getElementById('digExt');
+let empForm  = document.getElementById('empForm'); 
+let idEmployee = document.getElementById('idEmployee');
+let fullName = document.getElementById('fullName');
+let digExt = document.getElementById('digExt');
 let email = document.getElementById('email');
 let department = document.getElementById('department');
 
@@ -9,12 +9,14 @@ let department = document.getElementById('department');
 window.addEventListener('load', init, false)
    function init(){
        "use strict";
-   form.addEventListener("submit", () => {
+   empForm.addEventListener("submit", () => {
+       
         console.log('Employee id: '+  idEmployee.value);
         console.log('Full name: ' + fullName.value);
         console.log('Ext: '+ digExt.value);
         console.log('Email: ' + email.value);
         console.log('Department: ' +department.value);
         event.preventDefault();  
+        
 });
 }
